8a81ffd586ecc094b8db051b7b84b90fbd0a51a9
Nitish Kumar <krnitish@live.in>
2021-09-23 23:38:52 +0530

    JWT configured for User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,9 @@ const express = require("express")
 const router = express.Router()
 const { check, validationResult } = require('express-validator')
 const gravatar = require('gravatar')
+const jwt = require('jsonwebtoken')
 const bcrypt = require('bcryptjs')
+const config = require("config")
 const User = require('../../models/User')
 
 // * @route     POST api/users
@@ -50,7 +52,18 @@ router.post('/',[
         user.password = await bcrypt.hash(password, salt)
         await user.save()
         // TODO - return jsonwebtoken
-        res.send('User Registered')
+        const payload = {
+            user: {
+                id: user.id
+
+            }
+        }
+        jwt.sign(payload, config.get('jwtSecret'),
+        {expiresIn: 36000},
+        (err, token) => {
+            if (err) throw err
+            res.json({ token })
+        })
     } catch(err) {
         console.log(err.message)
         res.status(500).send('Server Error')
