4ecbc40308d5079f7dd8c3c220c9442bed59dec0
Nitish Kumar <krnitish@live.in>
2021-09-25 23:59:09 +0530

    View All Profile Route Added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -90,4 +90,18 @@ router.post('/', [auth, [
     }
 })
 
+// * @route  POST api/profile
+// * @desc   Get all profiles
+// * @access Public
+
+router.get('/', async (req, res) => {
+    try {
+        const profile = await Profile.find().populate('user', ['name', 'avatar'])
+        res.json(profile)
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
 module.exports = router;
