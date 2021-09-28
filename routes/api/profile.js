6a6d17333608ed610d7004979559bddf5024c840
Nitish Kumar <krnitish@live.in>
2021-09-28 23:33:18 +0530

    Delete Experience route added

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -196,4 +196,24 @@ router.put(
   }
 );
 
+// * @route  DELETE api/profile/experience/:exp_id
+// * @desc   Delete Profile Experience
+// * @access Private
+
+router.delete("/experience/:exp_id", auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+    // * Get remove Index
+    const removeIdx = profile.experience
+      .map((item) => item.id)
+      .indexOf(req.params.exp_id);
+    profile.experience.splice(removeIdx, 1);
+    await profile.save();
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
+
 module.exports = router;
