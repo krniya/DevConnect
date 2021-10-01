a14a0255bfc4885f697bef4ce1e676d977710de9
Nitish Kumar <krnitish@live.in>
2021-10-02 00:45:37 +0530

    Delete Post route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -56,7 +56,31 @@ router.get("/:id", auth, async (req, res) => {
     } catch (err) {
         console.log(err.message);
         if (err.kind == "ObjectId") {
-            return res.status(404).json({ msg: "Profile Not Found" });
+            return res.status(404).json({ msg: "Post Not Found" });
+        }
+        res.status(500).send("Server Error");
+    }
+});
+
+// * @route     DELETE api/posts/:id
+// * @desc      Delete a post by id
+// * @access    Private
+router.delete("/:id", auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            return res.status(404).json({ msg: "Post Not Found" });
+        }
+        // * Check User
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: "User not authorized" });
+        }
+        await post.remove();
+        res.json({ msg: "Post removed" });
+    } catch (err) {
+        console.log(err.message);
+        if (err.kind == "ObjectId") {
+            return res.status(404).json({ msg: "Post Not Found" });
         }
         res.status(500).send("Server Error");
     }
