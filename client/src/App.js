e33610508f620d8a914756ef1158168825bd4f0a
Nitish Kumar <krnitish@live.in>
2021-10-13 23:41:05 +0530

    Register import bug fixed

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Navbar } from "./components/lauout/Navbar";
 import { Landing } from "./components/lauout/Landing";
-import { Register } from "./components/auth/Register";
+import Register from "./components/auth/Register";
 import { Login } from "./components/auth/Login";
 import Alert from "./components/lauout/Alert";
 // * Redux
