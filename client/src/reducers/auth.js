3661c7696050b4e12ff37981f3d922a47e810310
Nitish Kumar <krnitish@live.in>
2021-10-14 01:05:03 +0530

    Register action added

---
+++ client/src/reducers/auth.js
@@ -0,0 +1,33 @@
+import { REGISTER_SUCESS, REGISTER_FAIL } from "../actions/types";
+
+const initialState = {
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default function (state = initialState, action) {
+    const { type, payload } = action;
+    switch (type) {
+        case REGISTER_SUCESS:
+            localStorage.setItem("token", payload.token);
+            return {
+                ...state,
+                ...payload,
+                isAuthenticated: true,
+                loading: false,
+            };
+        case REGISTER_FAIL:
+            localStorage.setItem("token");
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false,
+            };
+        default:
+            return state;
+    }
+}
