49a69518bcc2b4511ef7fb62a8a2956dfab9f43c
Nitish Kumar <krnitish@live.in>
2021-10-18 23:50:07 +0530

    Debugging auth issue

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -10,6 +10,7 @@ export const loadUser = () => async (dispatch) => {
     }
     try {
         const res = await axios.get("/auth/api");
+        console.log(res.data);
         dispatch({
             type: USER_LOADED,
             payload: res.data,
