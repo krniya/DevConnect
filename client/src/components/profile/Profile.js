4a686c4b04e265eb023c83e4d45d2df57eb9edc4
Nitish Kumar <krnitish@live.in>
2021-12-22 23:45:54 +0530

    Client user education added

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -6,6 +6,7 @@ import Spinner from "../lauout/Spinner";
 import { getProfileByID } from "../../actions/profile";
 import ProfileTop from "./ProfileTop";
 import ProfileExperince from "./ProfileExperience";
+import ProfileEducation from "./ProfileEducation";
 import ProfileAbout from "./ProfileAbout";
 
 const Profile = ({ getProfileByID, profile: { profile, loading }, auth, match }) => {
@@ -43,6 +44,21 @@ const Profile = ({ getProfileByID, profile: { profile, loading }, auth, match })
                                 </Fragment>
                             ) : (
                                 <h4>No Experience Credentials</h4>
+                            )}
+                        </div>
+                        <div className="profile-exp bg-white p-2">
+                            <h2 className="text-primary">Education</h2>
+                            {profile.education.length > 0 ? (
+                                <Fragment>
+                                    {profile.education.map((education) => (
+                                        <ProfileEducation
+                                            key={education._id}
+                                            education={education}
+                                        />
+                                    ))}
+                                </Fragment>
+                            ) : (
+                                <h4>No Education Credentials</h4>
                             )}
                         </div>
                     </div>
--- client/src/components/profile/ProfileExperience.js
+++ client/src/components/profile/ProfileExperience.js
@@ -5,19 +5,21 @@ import Moment from "react-moment";
 const ProfileExperience = ({
     experience: { company, title, location, current, to, from, description },
 }) => {
-    <div>
-        <h3 className="text-dark">{company}</h3>
-        <p>
-            <Moment format="YYYY/MM/DD">{from}</Moment> -
-            {!to ? " Now" : <Moment format="YYYY/MM/DD">{to}</Moment>}
-        </p>
-        <p>
-            <strong>Positon: </strong> {title}
-        </p>
-        <p>
-            <strong>description: </strong> {description}
-        </p>
-    </div>;
+    return (
+        <div>
+            <h3 className="text-dark">{company}</h3>
+            <p>
+                <Moment format="YYYY/MM/DD">{from}</Moment> -
+                {!to ? " Now" : <Moment format="YYYY/MM/DD">{to}</Moment>}
+            </p>
+            <p>
+                <strong>Positon: </strong> {title}
+            </p>
+            <p>
+                <strong>description: </strong> {description}
+            </p>
+        </div>
+    );
 };
 
 ProfileExperience.propTypes = {
